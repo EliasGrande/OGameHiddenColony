42202180b875b6671ec56c574157d5bafbf8117a
Elías Grande Cásedas <elias.grande.casedas@gmail.com>
2014-10-19 21:26:29 +0200

    Add icon

--- src/ogame-hidden-colony.user.js
+++ src/ogame-hidden-colony.user.js
@@ -4,6 +4,7 @@
 // @namespace    https://github.com/EliasGrande/
 // @downloadURL  https://github.com/EliasGrande/OGameHiddenColony/raw/master/dist/releases/latest.user.js
 // @updateURL    https://github.com/EliasGrande/OGameHiddenColony/raw/master/dist/releases/latest.meta.js
+// @icon         https://github.com/EliasGrande/OGameHiddenColony/raw/master/dist/img/icon.png
 // @version      1.0.1
 // @include      *://*.ogame.*/game/index.php?*page=*
 // ==/UserScript==
